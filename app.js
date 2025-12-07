a5fc5969330baa327c8ae708c995b8b24b6b2f7a
mayukhdeep12 <mayukhdeep12@gmail.com>
2025-12-08 01:37:30 +0530

    Hello

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
+// app.js
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from the CI Pipeline Demo!');
+  res.send('Hello from the CI Pipeline Demo!');
 });
 
 app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+  res.status(200).send('OK');
 });
 
 module.exports = app;
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
